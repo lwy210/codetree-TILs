c1c645590962ef8fd71a0263272f4127cc816e7e
lwy210 <33020581+lwy210@users.noreply.github.com>
2023-11-10 22:28:20 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 231110/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 231110/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -16,11 +16,13 @@ arr = sorted(arr, key = lambda x : x[0])
 # 감염 여부 체크
 for a in arr:
     t, x, y = map(int, a)
-    
-    if state[x] == 1 and klist[x] > 0:
+    if state[x] == 1 and klist[x] > 0 and state[y] == 1 and klist[y] > 0:
+        klist[x] -= 1
+        klist[y] -= 1
+    elif state[x] == 1 and klist[x] > 0:
         state[y] = 1
         klist[x] -= 1
-    if state[y] == 1 and klist[y] > 0:
+    elif state[y] == 1 and klist[y] > 0:
         state[x] = 1
         klist[y] -= 1
 
