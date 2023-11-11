6e869b142f58e004ce083a82d59a5166b54a6195
lwy210 <33020581+lwy210@users.noreply.github.com>
2023-11-11 21:51:33 +0900

    Title: 작은 구슬의 이동, Time: 158ms, Memory: 24MB, Status: Passed - CodeTree

--- 231111/작은 구슬의 이동/small-marble-movement.py
+++ 231111/작은 구슬의 이동/small-marble-movement.py
@@ -1,19 +1,20 @@
-def in_range(n, x, y):
-    return 0 <= x < n and 0 <= y < n
-
-dxs = [0, -1, 1, 0]
-dys = [1, 0, 0, -1]
-mapper = {'R':0, 'D':1, 'U':2, 'L':3}
-
 n, t = map(int, input().split())
 r, c, d = map(str, input().split())
+
+mapper = {'R':0, 'D':1, 'U':2, 'L':3}
 
 x, y = int(r)-1, int(c)-1 # r행 c열 구슬의 초기 위치
 n_dir = mapper[d] # 초기 방향
 
+dxs = [0, 1, -1, 0]
+dys = [1, 0, 0, -1]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
 for _ in range(t):
-    nx, ny = x + dxs[n_dir], y + dys[n_dir] # 1
-    if not in_range(n, nx, ny):
+    nx, ny = x + dxs[n_dir], y + dys[n_dir]
+    if not in_range(nx, ny):
         n_dir = 3 - n_dir
     else:
         x, y = nx, ny
