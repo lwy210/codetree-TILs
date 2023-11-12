393ab0749ddae23f36aa43825a61c2085bd723ff
lwy210 <33020581+lwy210@users.noreply.github.com>
2023-11-12 15:05:11 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231112/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
+++ 231112/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -1,12 +1,12 @@
 n, m = map(int, input().split())
 
 def in_range(x, y):
-    return 0 <= x < m and 0 <= y < n
+    return 0 <= x < n and 0 <= y < m
 
 # 남, 동, 북, 서
-dx = [0, 1, 0, -1]
-dy = [-1, 0, 1, 0]
-arr = [[0] * m for _ in range(n)]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+arr = [[0] * n for _ in range(m)]
 
 # 초기값
 n_dir = 0
@@ -16,13 +16,13 @@ x, y = 0, 0
 # 로직
 for i in range(2, n*m + 1):
     nx, ny = x + dx[n_dir], y + dy[n_dir]
-    if not in_range(nx, ny) or arr[ny][nx] != 0:
+    if not in_range(nx, ny) or arr[nx][ny] != 0:
         n_dir = (n_dir + 1) % 4
     x, y = x + dx[n_dir], y + dy[n_dir] # 여기를 x, y = nx, ny로 하면 안됨
-    arr[y][x] = i
+    arr[x][y] = i
 
 # 출력
 for i in range(m):
     for j in range(n):
-        print(arr[j][i], end=' ')
+        print(arr[i][j], end=' ')
     print()
