73492267151b061b318b7df9318e43cdc2492f8b
lwy210 <33020581+lwy210@users.noreply.github.com>
2023-11-25 22:31:59 +0900

    Title: 조건에 따른 연산, Time: 235ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231125/조건에 따른 연산/operatino-by-rule.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, cnt;
+
+        n = sc.nextInt();
+        cnt = 0;
+
+        while(n < 1000) {
+            if (n%2 == 0) n = n * 3 + 1;
+            else n = n * 2 + 2;
+            cnt += 1;
+        } System.out.print(cnt);
+    }
+}
